18302ef1cee662ea099e0dd0f0b4cb1e9abbe079
catriel25 <catrielconde@gmail.com>
2024-01-22 08:50:20 -0300

    update repo ML

--- modulo4/mercadoLiebre/app.js
+++ modulo4/mercadoLiebre/app.js
@@ -9,3 +9,6 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
+app.get('/register', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
